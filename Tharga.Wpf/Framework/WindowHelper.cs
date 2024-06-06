6361a9a710a1b41c60418c9b829d52df491643ba
Daniel Bohlin <daniel.bohlin@live.se>
2024-06-06 11:29:21 +0200

    normalize window when starting multiple instances

--- Tharga.Wpf/Framework/WindowHelper.cs
+++ Tharga.Wpf/Framework/WindowHelper.cs
@@ -5,9 +5,17 @@ namespace Tharga.Wpf.Framework;
 
 internal static class WindowHelper
 {
+    private const int SW_RESTORE = 9;
+
     [DllImport("user32.dll")]
     [return: MarshalAs(UnmanagedType.Bool)]
     private static extern bool SetForegroundWindow(nint hWnd);
+
+    [DllImport("user32.dll")]
+    private static extern bool IsIconic(IntPtr hWnd);
+
+    [DllImport("user32.dll")]
+    private static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
 
     public static void FocusWindowByProcessId(int processId)
     {
@@ -15,6 +23,11 @@ internal static class WindowHelper
         var mainWindowHandle = process.MainWindowHandle;
         if (mainWindowHandle != nint.Zero)
         {
+            if (IsIconic(mainWindowHandle))
+            {
+                ShowWindow(mainWindowHandle, SW_RESTORE);
+            }
+
             SetForegroundWindow(mainWindowHandle);
         }
     }
