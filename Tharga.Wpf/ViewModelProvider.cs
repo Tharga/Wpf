0072e7ac4968767d9f9bb448b349862e0dabd1ab
Daniel Bohlin <daniel.bohlin@live.se>
2024-06-06 13:37:20 +0200

    find view models in referenced projects

--- Tharga.Wpf/ViewModelProvider.cs
+++ Tharga.Wpf/ViewModelProvider.cs
@@ -1,5 +1,6 @@
 ﻿using System.Windows;
 using System.Windows.Markup;
+using Tharga.Toolkit.TypeService;
 
 namespace Tharga.Wpf;
 
@@ -15,7 +16,7 @@ public class ViewModelProvider : MarkupExtension
     public override object ProvideValue(IServiceProvider serviceProvider)
     {
         var applicationBase = (Application.Current as ApplicationBase);
-        var viewModel = applicationBase?.AppHost.Services.GetService(ViewModelType);
+        var viewModel = applicationBase?.AppHost.Services.GetService(ViewModelType) ?? AssemblyService.GetTypes(x => x == ViewModelType).FirstOrDefault();
         if (viewModel == null) throw new TypeNotRegisteredException(ViewModelType, applicationBase == null ? $" Application.Current is '{Application.Current.GetType().FullName}', it needs to be of type {nameof(ApplicationBase)}." : null);
         return viewModel;
     }
