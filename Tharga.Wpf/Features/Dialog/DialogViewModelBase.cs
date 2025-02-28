ede667a8906bf05547a654694b30a4184658cfa6
Daniel Bohlin <daniel.bohlin@live.se>
2025-02-28 14:38:46 +0100

    possible to override dialog close action

--- Tharga.Wpf/Features/Dialog/DialogViewModelBase.cs
+++ Tharga.Wpf/Features/Dialog/DialogViewModelBase.cs
@@ -6,12 +6,17 @@ public class DialogViewModelBase : ViewModelBase
 {
     public DialogViewModelBase()
     {
-        OkCommand = new RelayCommand(_ => { RequestCloseEvent?.Invoke(this, new RequestCloseEvent(true)); }, _ => true);
-        CancelCommand = new RelayCommand(_ => { RequestCloseEvent?.Invoke(this, new RequestCloseEvent(false)); }, _ => true);
+        OkCommand = new RelayCommand(_ => { RequestClose(true); }, _ => true);
+        CancelCommand = new RelayCommand(_ => { RequestClose(false); }, _ => true);
     }
 
     public event EventHandler<RequestCloseEvent> RequestCloseEvent;
 
-    public ICommand OkCommand { get; }
-    public ICommand CancelCommand { get; }
+    protected void RequestClose(bool dialogResult)
+    {
+        RequestCloseEvent?.Invoke(this, new RequestCloseEvent(dialogResult));
+    }
+
+    public ICommand OkCommand { get; protected set; }
+    public ICommand CancelCommand { get; protected set; }
 }
